64649fb9baadd81c932985f57596b81822901ddd
João Paulo S. Costa <43136205+kjuao9@users.noreply.github.com>
2019-06-25 15:52:07 -0300

    1.2.256 -> Consultas/Teste

--- modelo php bd/testes/consultas_teste.php
+++ modelo php bd/testes/consultas_teste.php
@@ -1,4 +1,27 @@
 <?php
 include "../conexao.php";
+$sql = "SELECT * FROM usuarios";
+$con = conecta_mysql();
+$resultado = mysqli_query($con, $sql);
+if($resultado){
+    $usuarios = array();
+    //todo bd q tiver mais de um valor pode fazer isso pra mostrar os valoresss
+    while( $linha = mysqli_fetch_assoc($resultado) ){
+        $usuarios[] = $linha;
+    }
+    
 
+//abaixo mostra tudo
+print "<pre>";
+    print_r($usuarios);
+print "</pre>";
+
+//abaixo mostra só o nome
+foreach($usuarios as $usuario){
+    print "Nome: ";
+    print $usuario["nome"];
+    print "<br/>";
+}
+
+}
 ?>
